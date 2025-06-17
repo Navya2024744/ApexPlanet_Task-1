caa99a5cd0f82b1666a1d7c174cb190e06d8e2b1
Navya2024744 <navyasrimekapothula@gmail.com>
2025-06-17 22:57:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the button using its ID
+const alertButton = document.getElementById('alertBtn');
+
+// Add a click event listener to the button
+alertButton.addEventListener('click', () => {
+    alert('Hello! You clicked the button.');
+});
